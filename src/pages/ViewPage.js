12021ef7baeab0a4d44f7744cedc1c468c162166
Max <max@ref.finance>
2023-05-02 01:15:21 +0800

    fix url bug

--- src/pages/ViewPage.js
+++ src/pages/ViewPage.js
@@ -39,9 +39,6 @@ export default function ViewPage(props) {
             }
       );
 
-      if (src === props.widgets.componentsPage) {
-        window.location.replace('/#/' + src);
-      }
       analytics('view', {
         props: {
           widget: src,
