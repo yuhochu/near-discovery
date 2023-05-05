172febbf2ded3fb694f9411cc792320fdf3deea6
Max <max@ref.finance>
2023-05-05 14:35:59 +0800

    update near social vm

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const webpack = require("webpack");
-const paths = require("./config/paths");
-const path = require("path");
-const ManifestPlugin = require("webpack-manifest-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const { merge } = require("webpack-merge");
-const loadPreset = require("./config/presets/loadPreset");
-const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+const webpack = require('webpack');
+const paths = require('./config/paths');
+const path = require('path');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { merge } = require('webpack-merge');
+const loadPreset = require('./config/presets/loadPreset');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const loadConfig = (mode) => require(`./config/webpack.${mode}.js`)(mode);
 
 module.exports = function (env) {
-  const { mode = "production" } = env || {};
+  const { mode = 'production' } = env || {};
   return merge(
     {
       mode,
       entry: `${paths.srcPath}/index.js`,
       output: {
         path: paths.distPath,
-        filename: "[name].bundle.js",
-        publicPath: "/",
+        filename: '[name].bundle.js',
+        publicPath: '/',
       },
       module: {
         rules: [
@@ -31,33 +31,35 @@ module.exports = function (env) {
           },
           {
             test: /\.js$/,
-            use: ["babel-loader"],
-            exclude: path.resolve(__dirname, "node_modules"),
+            use: ['babel-loader'],
+            exclude: path.resolve(__dirname, 'node_modules'),
           },
           // Images: Copy image files to build folder
-          { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: "asset/resource" },
+          { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
 
           // Fonts and SVGs: Inline files
-          { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: "asset/inline" },
+          { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
         ],
       },
       resolve: {
-        modules: [paths.srcPath, "node_modules"],
-        extensions: [".js", ".jsx", ".json"],
+        modules: [paths.srcPath, 'node_modules'],
+        extensions: ['.js', '.jsx', '.json'],
         fallback: {
-          crypto: require.resolve("crypto-browserify"),
-          stream: require.resolve("stream-browserify"),
+          crypto: require.resolve('crypto-browserify'),
+          stream: require.resolve('stream-browserify'),
+          http: require.resolve('stream-http'),
+          https: require.resolve('https-browserify'),
         },
         // Fix for using `yarn link "near-social-vm"`
         alias: {
-          react: path.resolve(__dirname, "./node_modules/react"),
-          "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+          react: path.resolve(__dirname, './node_modules/react'),
+          'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
         },
       },
       plugins: [
         new webpack.EnvironmentPlugin({
           // Configure environment variables here.
-          ENVIRONMENT: "browser",
+          ENVIRONMENT: 'browser',
         }),
         new CleanWebpackPlugin(),
         // Copies files from target to destination folder
@@ -65,9 +67,9 @@ module.exports = function (env) {
           patterns: [
             {
               from: paths.publicPath,
-              to: "assets",
+              to: 'assets',
               globOptions: {
-                ignore: ["*.DS_Store"],
+                ignore: ['*.DS_Store'],
               },
               noErrorOnMissing: true,
             },
@@ -80,8 +82,8 @@ module.exports = function (env) {
         }),
         new webpack.ProgressPlugin(),
         new webpack.ProvidePlugin({
-          process: "process/browser",
-          Buffer: [require.resolve("buffer/"), "Buffer"],
+          process: 'process/browser',
+          Buffer: [require.resolve('buffer/'), 'Buffer'],
         }),
         new ManifestPlugin.WebpackManifestPlugin(),
       ],
